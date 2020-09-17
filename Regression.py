ac398244cb7d25a1a5d3825c662a67e1f947df86
jonmartz <jonmartz91@gmail.com>
2020-09-17 17:30:03 +0300

    Added Cobra and BagBoo to pipeline, achieved good performance

--- Regression.py
+++ Regression.py
@@ -2,20 +2,18 @@ import os
 import csv
 import numpy as np
 import pandas as pd
-from time import time
+from time import time, perf_counter
 from sklearn.model_selection import KFold
 from sklearn.model_selection import RandomizedSearchCV
 from scipy.stats import uniform, randint
 from sklearn import metrics
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import AdaBoostRegressor, RandomForestRegressor
 from pycobra.cobra import Cobra
 from pycobra.ewa import Ewa
 from pycobra.diagnostics import Diagnostics
-
-
 # from algorithms.BagBoo import BagBoo
 
 
@@ -34,11 +32,12 @@ def get_time_string(time_in_seconds):
 
 
 num_folds = 10
-random_search_iters = 10
+random_search_iters = 50
+grid_points = 20  # for hyper-params optimization in Cobra and Ewa
 random_search_cv = 3
 models = [
-    # {'name': 'AdaBoost', 'model': AdaBoostRegressor(DecisionTreeRegressor(random_state=1), random_state=1),
-    #  'rs_params': {'n_estimators': randint(5, 100), 'base_estimator__ccp_alpha': uniform(0, 0.1)}},
+    {'name': 'AdaBoost', 'model': AdaBoostRegressor(DecisionTreeRegressor(random_state=1), random_state=1),
+     'rs_params': {'n_estimators': randint(5, 100), 'base_estimator__ccp_alpha': uniform(0, 0.1)}},
 
     # {'name': 'RandomForest', 'model': RandomForestRegressor(random_state=1),
     #  'rs_params': {'n_estimators': randint(5, 50), 'ccp_alpha': uniform(0, 0.01)}},
@@ -78,12 +77,12 @@ for dataset_idx, file in enumerate(files):
     # load and pre-process dataset
     dataset_name = os.fsdecode(file)[:-4]
     dataset = pd.read_csv('datasets/%s.csv' % dataset_name)
-    # dataset = dataset.fillna(method='ffill').fillna(method='bfill')  # fill nan values
     dataset = dataset.fillna(dataset.mean())  # fill nan values
-    dataset = pd.get_dummies(dataset)  # one-hot encode categorical features
-    array = dataset.to_numpy()
-    # array = StandardScaler().fit_transform(array)
-    X, y = array[:, :-1], array[:, -1]
+    target_col = dataset.columns[-1]
+    X = pd.get_dummies(dataset.drop(columns=target_col)).to_numpy()  # one-hot encode categorical features
+    y = dataset[target_col].to_numpy()
+    X = MinMaxScaler().fit_transform(X)
+    y = MinMaxScaler().fit_transform(y.reshape(-1, 1)).ravel()
 
     # start k-fold cross validation
     folds = list(KFold(n_splits=num_folds, shuffle=True, random_state=1).split(dataset))
@@ -104,33 +103,35 @@ for dataset_idx, file in enumerate(files):
                 if model['name'] in ['Cobra', 'Ewa']:
                     # fit model
                     best_model = model['model'](random_state=1)
-                    start_time_train = time()
-                    np.random.seed(1)
+                    start_time_train = perf_counter()
+                    # np.random.seed(1)
                     # val_idx = np.random.choice(X_train.shape[0], int(X_train.shape[0] * 0.2), replace=False)
                     # X_val, y_val = X_train[val_idx, :], y_train[val_idx]
                     X_val, y_val = X_train, y_train
                     if model['name'] == 'Cobra':
-                        best_model.set_epsilon(X_epsilon=X_val, y_epsilon=y_val, grid_points=random_search_iters)
+                        best_model.set_epsilon(X_epsilon=X_val, y_epsilon=y_val, grid_points=grid_points)
                         best_parameters = {'epsilon': best_model.epsilon}
                     else:  # Ewa
                         best_model.set_beta(X_beta=X_val, y_beta=y_val)
                         best_parameters = {'beta': best_model.beta}
                     best_model.fit(X_train, y_train)
-                    runtime_train = time() - start_time_train
+                    runtime_train = perf_counter() - start_time_train
                 else:
                     best_model = RandomizedSearchCV(model['model'], model['rs_params'], random_state=1,
-                                                    n_iter=random_search_iters, cv=random_search_cv)
-                    start_time_train = time()
+                                                    n_iter=grid_points, cv=random_search_cv)
+                    start_time_train = perf_counter()
                     best_model.fit(X_train, y_train)
-                    runtime_train = time() - start_time_train
+                    runtime_train = perf_counter() - start_time_train
                     best_parameters = best_model.best_params_
 
-                # test model
-                start_time_test = time()
-                y_pred = best_model.predict(X_test)
-                runtime_test = time() - start_time_test
+                # inference time
+                X_inference = X_test[np.random.randint(0, len(X_test), 1000)]  # normalize to 1000 samples
+                start_time_test = perf_counter()
+                best_model.predict(X_inference)
+                runtime_test = perf_counter() - start_time_test
 
                 # compute metrics and prepare log entry
+                y_pred = best_model.predict(X_test)
                 row = [dataset_name, model['name'], fold_idx + 1, best_parameters,
                        metrics.mean_squared_error(y_test, y_pred),
                        metrics.mean_absolute_error(y_test, y_pred),
@@ -152,5 +153,5 @@ for dataset_idx, file in enumerate(files):
                     iteration, iterations, dataset_idx + 1, len(files), dataset_name, fold_idx + 1, len(folds),
                     model_idx + 1, len(models), model['name'], get_time_string(runtime), eta)
                 print(progress_row)
-                with open('results/progress_log.txt', 'a') as file:
-                    file.write('%s\n' % progress_row)
+                with open('results/progress_log.txt', 'a') as prog_file:
+                    prog_file.write('%s\n' % progress_row)
